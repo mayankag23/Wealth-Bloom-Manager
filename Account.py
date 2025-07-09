107d3ea0e03c0f8e2d4368fdec674c90ae59cecd
Mayank Agrawal <153766256+mayankag23@users.noreply.github.com>
2025-07-10 01:03:11 +0530

    Create Account.py

---
+++ Account.py
@@ -0,0 +1,7 @@
+class Account:
+  def __init__(self, account_id, user_id, password, admin, amount):
+    self.account_id = account_id
+    self.user_id = user_id
+    self.password = password
+    self.admin = admin
+    self.amount = amount
